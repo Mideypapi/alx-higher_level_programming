d1d630c1b387cdcb943befd617f292f7627687d6
Mideypapi <abioye.faruok@gmail.com>
2024-01-03 03:37:06 -0800

    first

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
+ * check_cycle - checks if the linked list contains d cycle
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
